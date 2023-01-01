2c4574e9ed10004709e9271768a1a602f1d83dad
Kamil Prokop <zara.pug.yt@gmail.com>
2023-01-01 22:27:37 +0100

    Last sentence

--- lista_zakupow_git.py
+++ lista_zakupow_git.py
@@ -4,5 +4,11 @@ lista_zakupow = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 for shop, product in lista_zakupow.items():
-    print("Idę do", shop, "i kupuję tam", product)
+    print("Idę do", shop.capitalize(), "i kupuję tam", product.title())
 
+counting = 0
+for shop, product in lista_zakupow.items():
+    for i in product:
+        counting = counting + 1
+
+print("W sumie kupuję", counting, "produktów.")
