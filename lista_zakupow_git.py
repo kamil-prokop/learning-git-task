747218c2cb6e55b3a001009db0e56383d0d90f07
Kamil Prokop <zara.pug.yt@gmail.com>
2023-01-01 22:23:06 +0100

    Init main text

--- lista_zakupow_git.py
+++ lista_zakupow_git.py
@@ -1,4 +1,7 @@
+lista_zakupow = dict
 lista_zakupow = { 
     "piekarnia": ["chleb", "pączek", "bułki"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+for shop, product in lista_zakupow.items():
+    print("Idę do", shop, "i kupuję tam", product)
